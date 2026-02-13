95a6e8c6272c24446765a6eb5697cba619e5b61c
palaciojoel395-coder <palaciojoel395@gmail.com>
2026-02-12 22:54:07 -0500

    corrigiendo error en css

--- app.py
+++ app.py
@@ -2,12 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Página principal
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Página de pronunciación
 @app.route('/pronunciacion')
 def pronunciacion():
     return render_template('pronunciacion.html')
