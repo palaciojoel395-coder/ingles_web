92f76d9fba32300330934bfad31542b41ed3c056
palaciojoel395-coder <palaciojoel395@gmail.com>
2026-02-17 11:59:09 -0500

    332

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ def inicio():
 def pronunciacion():
     return render_template("pronunciacion.html")
 
+@app.route("/interaccion y aprendizaje")
+def interaccion():
+    return render_template("interaccion y aprendizaje.html")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000, debug=True)
